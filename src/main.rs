98fc7dd9ecafab41335340e5b71dfcfb776104fa
RobbieClarken <robbie.clarken@gmail.com>
2019-02-04 22:04:26 +1100

    Add remaining fields to extern_proc struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 #![feature(untagged_unions)]
 #![allow(non_camel_case_types)]
 
-use libc;
-use libc::{c_void, c_int};
+use libc::{c_char, c_int, c_long, c_uchar, c_uint, c_ushort, c_void};
+use std::ffi::CStr;
 use std::mem;
 use std::ptr;
-use std::ffi::CStr;
-
 
 const MAXCOMLEN: usize = 16;
-const KINFO_PROC_LEN: usize = 648;
-const KINFO_KP_PROC_IMPL_LEN: usize = 264;
 const KINFO_KP_EPROC_LEN: usize = 352;
 
 #[repr(C)]
@@ -21,7 +17,7 @@ struct sleep_queue {
 
 #[repr(C)]
 struct timeval {
-    tv_sec: libc::c_long,
+    tv_sec: c_long,
     tv_usec: i32,
 }
 
@@ -30,7 +26,6 @@ union sleep_queue_start_time_union {
     __p_st1: sleep_queue,
     __p_starttime: timeval,
 }
-
 
 #[repr(C)]
 struct itimerval {
@@ -76,7 +71,7 @@ struct extern_proc {
     sigwait: bool,
     // /* scheduling */
     // u_int  p_estcpu;   /* Time averaged value of p_cpticks. */
-    p_estcpu: libc::c_uint,
+    p_estcpu: c_uint,
     // int  p_cpticks;   /* Ticks of cpu time. */
     p_cpticks: c_int,
     // fixpt_t  p_pctcpu;   /* %cpu for this process during p_swtime */
@@ -84,11 +79,11 @@ struct extern_proc {
     // void  *p_wchan;   /* Sleep address. */
     p_wchan: *const c_void,
     // char  *p_wmesg;   /* Reason for sleep. */
-    p_wmesg: *const libc::c_void,
+    p_wmesg: *const c_void,
     // u_int  p_swtime;   /* Time swapped in or out. */
-    p_swtime: libc::c_uint,
+    p_swtime: c_uint,
     // u_int  p_slptime;   /* Time since last blocked. */
-    p_slptime: libc::c_uint,
+    p_slptime: c_uint,
     // struct  itimerval p_realtimer;  /* Alarm timer. */
     p_realtimer: itimerval,
     // struct  timeval p_rtime;  /* Real time. */
@@ -102,44 +97,45 @@ struct extern_proc {
     // int  p_traceflag;    /* Kernel trace points. */
     p_traceflag: c_int,
     // // struct  vnode *p_tracep;  /* Trace to vnode. */
-    p_tracep: *const libc::c_void,
+    p_tracep: *const c_void,
     // int  p_siglist;    /* DEPRECATED. */
     p_siglist: c_int,
     // // struct  vnode *p_textvp;  /* Vnode of executable. */
-    p_textvp: *const libc::c_void,
+    p_textvp: *const c_void,
     // int  p_holdcnt;    /* If non-zero, don't swap. */
     p_holdcnt: c_int,
     // sigset_t p_sigmask;  /* DEPRECATED. */
-    // XXX <- should have offset 228
     p_sigmask: sigset_t,
     // sigset_t p_sigignore;  /* Signals being ignored. */
     p_sigignore: sigset_t,
     // sigset_t p_sigcatch;  /* Signals being caught by user. */
     p_sigcatch: sigset_t,
     // u_char  p_priority;  /* Process priority. */
-    p_priority: libc::c_uchar,
+    p_priority: c_uchar,
     // // u_char  p_usrpri;  /* User-priority based on p_cpu and p_nice. */
-    p_usrpri: libc::c_uchar,
+    p_usrpri: c_uchar,
     // char  p_nice;    /* Process "nice" value. */
-    p_nice: libc::c_char,
+    p_nice: c_char,
     // char  p_comm[MAXCOMLEN+1];
-    p_comm: [libc::c_char; MAXCOMLEN + 1],
-    _padding: [libc::c_uchar; KINFO_PROC_LEN - KINFO_KP_PROC_IMPL_LEN - KINFO_KP_EPROC_LEN],
+    p_comm: [c_char; MAXCOMLEN + 1],
     // struct   pgrp *p_pgrp;  /* Pointer to process group. */
+    p_pgrp: *const c_void,
     // struct  user *p_addr;  /* Kernel virtual addr of u-area (PROC ONLY). */
+    p_addr: *const c_void,
     // u_short  p_xstat;  /* Exit status for wait; also stop signal. */
+    p_xstat: c_ushort,
     // u_short  p_acflag;  /* Accounting flags. */
+    p_acflag: c_ushort,
     // struct  rusage *p_ru;  /* Exit information. XXX */
+    p_ru: *const c_void,
 }
 
 struct kinfo_proc {
     kp_proc: extern_proc,
-    _kp_eproc: [libc::c_uchar; KINFO_KP_EPROC_LEN],
+    _kp_eproc: [c_uchar; KINFO_KP_EPROC_LEN],
 }
 
 fn main() {
-    // dbg!(mem::size_of::<extern_proc>());
-    // panic!();
     let mut name: [c_int; 3] = [libc::CTL_KERN, libc::KERN_PROC, libc::KERN_PROC_ALL];
     let mut length: libc::size_t = 0;
     let mut err = unsafe {
@@ -157,13 +153,13 @@ fn main() {
         panic!();
     }
 
-    let mut buf: Vec<libc::c_uchar> = vec![0; length];
+    let mut buf: Vec<c_uchar> = vec![0; length];
 
     err = unsafe {
         libc::sysctl(
             name.as_mut_ptr(),
             name.len() as u32,
-            buf.as_mut_ptr() as *mut libc::c_void,
+            buf.as_mut_ptr() as *mut c_void,
             &mut length as *mut libc::size_t,
             ptr::null_mut(),
             0,
@@ -173,7 +169,7 @@ fn main() {
     if err != 0 {
         panic!();
     }
-    let struct_ptr = buf.as_mut_ptr() as *const kinfo_proc;
+    let struct_ptr = buf.as_ptr() as *const kinfo_proc;
     let n = length / mem::size_of::<kinfo_proc>();
     let procs = unsafe { std::slice::from_raw_parts(struct_ptr, n) };
 
@@ -188,7 +184,7 @@ fn main() {
             libc::sysctl(
                 name.as_mut_ptr(),
                 name.len() as u32,
-                &mut argmax as *mut _ as *mut libc::c_void,
+                &mut argmax as *mut _ as *mut c_void,
                 &mut size as *mut libc::size_t,
                 ptr::null_mut(),
                 0,
@@ -200,12 +196,12 @@ fn main() {
 
         let pid = proc.kp_proc.p_pid;
         let mut name: [c_int; 3] = [libc::CTL_KERN, libc::KERN_PROCARGS2, pid];
-        let mut buf: Vec<libc::c_uchar> = vec![0; argmax as usize];
+        let mut buf: Vec<c_uchar> = vec![0; argmax as usize];
         err = unsafe {
             libc::sysctl(
                 name.as_mut_ptr(),
                 name.len() as u32,
-                buf.as_mut_ptr() as *mut libc::c_void,
+                buf.as_mut_ptr() as *mut c_void,
                 &mut argmax as *mut _ as *mut libc::size_t,
                 ptr::null_mut(),
                 0,
@@ -219,7 +215,8 @@ fn main() {
             off += mem::size_of_val(&nargs);
             let exec_path = unsafe { CStr::from_ptr(buf_ptr.add(off) as *const i8) };
             off += exec_path.to_bytes().len() + 1;
-            while unsafe { buf_ptr.add(off).read() } == 0 {  // should impl some check that off isn't beyond end of buf
+            while unsafe { buf_ptr.add(off).read() } == 0 {
+                // should impl some check that off isn't beyond end of buf
                 off += 1;
             }
             for _ in 0..nargs {
