e72139b75d47a799da2e7fac7d4356838862c73d
RobbieClarken <robbie.clarken@gmail.com>
2019-02-09 22:18:49 +1100

    Use docopt to specify and parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 #![feature(untagged_unions)]
 
+use docopt::Docopt;
+use serde_derive::Deserialize;
+
 mod processes;
 
+const USAGE: &'static str = "
+procgrep
+
+Usage:
+  procgrep <pattern>
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_pattern: String,
+}
+
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
     for process in processes::get_processes() {
-        println!("{}", process.pid);
+        if process.command.contains(&args.arg_pattern) {
+            println!("{}", process.pid);
+        }
     }
 }
